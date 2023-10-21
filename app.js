c0da02e538d19aee2c92b6aa86d5d711648b0076
NandiniGite <98202333+NandiniGite@users.noreply.github.com>
2023-10-21 20:00:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express=require("express");
 const app=express();
 
+const cors = require('cors');
+app.use(cors());
 require("dotenv").config();
 const connect=require('./db/connect');
 const PORT=process.env.PORT || 5000;
